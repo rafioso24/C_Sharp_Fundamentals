2b0461f917ab3791e040c2a74fdbfd2d6d97541d
Rafael <rdmoas@UPS6L17MKMK32.upstreamaccts.XOM.com>
2015-10-29 12:06:51 -0500

    new project

---
+++ Grades/Grades/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GradeBook book = new GradeBook();  //invoke the constructor to make a new Gradebook object
+            book.AddGrade(91);
+            book.AddGrade(89.5f);               // apply an 'f' to show that it's a floating point number
+        }
+    }
+}
