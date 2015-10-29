d2a699965576d0371121cce343af75234c7bfff4
Rafael <rdmoas@UPS6L17MKMK32.upstreamaccts.XOM.com>
2015-10-29 11:45:06 -0500

    Finished Module 1

--- Hello/Hello/Program.cs
+++ Hello/Hello/Program.cs
@@ -8,7 +8,21 @@ namespace Hello
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello, " + args[0] + "world");
+            Console.WriteLine("Your Name:");
+            String name = Console.ReadLine();
+            
+            Console.WriteLine("How Many hours of sleep did you get last night?");
+            int hoursOfSleep = int.Parse( Console.ReadLine() );                 //parse converts string into integer
+
+            Console.WriteLine("hello, " + name);
+            if (hoursOfSleep > 8){
+                Console.WriteLine("you are well rested.");
+            }
+            else
+            {
+                Console.WriteLine("you need more sleep.");
+            }
+           
         }
     }
 }
