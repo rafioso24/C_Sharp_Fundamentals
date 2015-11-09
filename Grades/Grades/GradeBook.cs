90098bd0dd5117c6da553e605ff15c6ed1ca565f
Rafael <rdmoas@UPS6L17MKMK32.upstreamaccts.XOM.com>
2015-11-09 10:43:14 -0600

    encapsulations, statics

--- Grades/Grades/GradeBook.cs
+++ Grades/Grades/GradeBook.cs
@@ -11,10 +11,24 @@ namespace Grades
         public GradeBook() 
         {
         }
+        public GradeStatistics ComputeStatistics()
+        {
+            GradeStatistics stats =  new GradeStatistics();
+            float sum = 0;
+            foreach (float grade in grades)
+            {
+                stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
+                stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
+                sum += grade;
+            }
+            stats.AverageGrade = sum / grades.Count;
+
+            return stats;
+        }
         public void AddGrade(float grade){
             grades.Add(grade);
             
         }
-        List<float> grades = new List<float>(); //imediately initialize this field to a new list
+        private List<float> grades = new List<float>(); //imediately initialize this field to a new list
     }
 }
--- Grades/Grades/Program.cs
+++ Grades/Grades/Program.cs
@@ -12,9 +12,13 @@ namespace Grades
         {
             GradeBook book = new GradeBook();  //invoke the constructor to make a new Gradebook object
             book.AddGrade(91);
-            book.AddGrade(89.5f);               // apply an 'f' to show that it's a floating point number
-            book = new GradeBook();
+            book.AddGrade(89.5f);     // apply an 'f' to show that it's a floating point number
             book.AddGrade(75);
+
+            GradeStatistics stats = book.ComputeStatistics();
+            Console.WriteLine(stats.AverageGrade);
+            Console.WriteLine(stats.HighestGrade);
+            Console.WriteLine(stats.LowestGrade);
         }
     }
 }
